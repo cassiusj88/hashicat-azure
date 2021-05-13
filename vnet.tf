d4b8c6a517db21f217eeb214312664c1a6c73e08
cassiusj88 <joshuarstevens@gmail.com>
2021-05-13 19:00:59 +0000

    updated rg name to cassius-training

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/cassius-training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name = "network"
+  resource_group_name = "cassius-training"
 }
